46bdaff40e555c5e2e8d54a3043e9d7fb61db849
Andrew Rademacher <andrew.rademacher@streamix.co>
2013-04-27 17:23:47 -0500

    -Added color list.

--- app/scripts/controllers/ulam.js
+++ app/scripts/controllers/ulam.js
@@ -2,5 +2,29 @@
 
 angular.module('playgroundApp')
     .controller('UlamCtrl', function ($scope) {
-
+        $scope.seedId = 5;
+        $scope.width = 1;
+        $scope.colors = [
+            {id: 0, value: 'red'},
+            {id: 1, value: 'blue'},
+            {id: 2, value: 'green'}
+        ];
+        $scope.newColor = 'black';
+        $scope.paper = new Raphael(document.getElementById('ulam-canvas-container'), 600, 600);
+        $scope.addColor = function () {
+            $scope.colors.push({
+                id: $scope.seedId++,
+                value: $scope.newColor
+            });
+        };
+        $scope.removeColor = function (colorId) {
+            $scope.colors = _.reject($scope.colors, function (color) {
+                return color.id === colorId;
+            });
+        };
+        $scope.draw = function () {
+            var circle = $scope.paper.circle(250, 250, 100);
+            circle.attr('stroke', 'black');
+            circle.attr('fill', 'red');
+        };
     });
